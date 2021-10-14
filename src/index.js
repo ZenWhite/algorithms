bd405a0a96309a269febe89e6ef492996b83403b
Andrew Letyuchiy <misterbook13@mail.ru>
2021-10-14 22:53:45 +0300

    chage to my reduce

--- src/index.js
+++ src/index.js
@@ -29,4 +29,4 @@ const reduceRes = reduce(
   {}
 )
 
-console.log(reduceRes)
+console.log(flatRes)
--- src/main/recursion/flat.js
+++ src/main/recursion/flat.js
@@ -1,6 +1,8 @@
+import { reduce } from 'main/recursion/reduce'
+
 export const flat = (list) =>
-  list.reduce(
-    (list, current) =>
-      list.concat(Array.isArray(current) ? flat(current) : [current]),
+  reduce(
+    (acc, item) => acc.concat(Array.isArray(item) ? flat(item) : [item]),
+    list,
     []
   )
